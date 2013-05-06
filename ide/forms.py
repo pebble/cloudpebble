e68854eb28523fe1120ec86fc7ea33fff608b951
Katharine Berry <katharine@kathar.in>
2013-05-06 11:52:17 -0400

    Fix user field.

--- ide/forms.py
+++ ide/forms.py
@@ -6,4 +6,4 @@ from ide.models import UserSettings
 class SettingsForm(ModelForm):
     class Meta:
         model = UserSettings
-        include = ('autocomplete', 'keybinds', 'theme')
+        exclude = ('user',)
