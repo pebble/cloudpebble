ee0df48a0f9d3c007067ec56cf60ce30ab296e7c
Katharine Berry <katharine@kathar.in>
2013-11-06 06:11:57 -0500

    wscript changes to build with latest versions.

--- ide/tasks.py
+++ ide/tasks.py
@@ -231,13 +231,20 @@ top = '.'
 out = 'build'
 
 def options(ctx):
-  ctx.load('pebble_sdk')
+    ctx.load('pebble_sdk')
 
 def configure(ctx):
-  ctx.load('pebble_sdk')
+    ctx.load('pebble_sdk')
 
 def build(ctx):
-  ctx.load('pebble_sdk')
+    ctx.load('pebble_sdk')
+
+    ctx.pbl_program(source=ctx.path.ant_glob('src/**/*.c'),
+                    target='pebble-app.elf')
+
+    ctx.pbl_bundle(elf='pebble-app.elf',
+                   js=ctx.path.ant_glob('src/js/**/*.js'))
+
 """
 
 @task(acks_late=True)
