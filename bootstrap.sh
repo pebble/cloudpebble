47b76d8d0cffd5a34ae8f6614e494456d0fa22d6
Katharine Berry <katharine@kathar.in>
2014-01-18 17:16:40 -0500

    Changed to Pebble's beta 6 download.

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,8 +41,7 @@ popd
 
 sudo -u vagrant mkdir sdk2
 pushd sdk2
-    # Host this ourselves until Pebble stops being silly.
-    wget --progress=bar:force -O sdk.tar.gz http://cloudpebble-vagrant.s3.amazonaws.com/PebbleSDK-2.0-BETA5.tar.gz
+    wget --progress=bar:force -O sdk.tar.gz https://s3.amazonaws.com/assets.getpebble.com/sdk2/PebbleSDK-2.0-BETA6.tar.gz
     sudo -u vagrant tar --strip 1 -xzf sdk.tar.gz
     rm sdk.tar.gz
     sudo -u vagrant ln -s ~/arm-cs-tools arm-cs-tools
