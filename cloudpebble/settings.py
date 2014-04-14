3e82157f2b19218084fe1b686c876c875b9581d1
Katharine Berry <katharine@getpebble.com>
2014-04-13 22:25:42 -0700

    staticfiles for Heroku.

--- cloudpebble/settings.py
+++ cloudpebble/settings.py
@@ -25,6 +25,9 @@ DATABASES = {
     }
 }
 
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__)) + '/../'
+
+
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
@@ -79,7 +82,7 @@ MEDIA_URL = 'http://localhost:8001/builds/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
