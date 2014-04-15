5f7b20fa02a8d8b32be2f92e6b10fd3f65ea0a69
Katharine Berry <katharine@getpebble.com>
2014-04-15 07:59:11 -0700

    Implemented buttons to hide/show split UI with animation.

---
+++ root/static/root/js/splash.js
@@ -0,0 +1,11 @@
+$(function() {
+    var gMainContent = $('.main-container');
+
+    $('.btn-show-login').click(function() {
+        gMainContent.addClass('show-login');
+    });
+
+    $('.btn-hide-login').click(function() {
+        gMainContent.removeClass('show-login');
+    });
+});
