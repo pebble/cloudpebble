21db48f5d2ea61e30c5582002bb4ccd9b9b0315f
Katharine Berry <katharine@getpebble.com>
2014-04-15 08:34:11 -0700

    Added links and hover states for some buttons.

--- root/views.py
+++ root/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-
+from django.conf import settings
 
 def index(request):
     if request.user.is_authenticated():
         return redirect("/ide/")
     else:
-        return render(request, 'root/index.html')
+        return render(request, 'root/index.html', {'sso_root': settings.SOCIAL_AUTH_PEBBLE_ROOT_URL})
