27a7c3256d92e8da70b266e1984fae2c34291ee3
Katharine Berry <katharine@getpebble.com>
2014-04-18 17:16:22 -0700

    Don't fail to launch when AWS is disabled. Closes #69.

--- utils/s3.py
+++ utils/s3.py
@@ -2,27 +2,43 @@ import boto
 from boto.s3.key import Key
 from django.conf import settings
 
-_s3 = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+if settings.AWS_ENABLED:
+    _s3 = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
 
-_buckets = {
-    'source': _s3.get_bucket(settings.AWS_S3_SOURCE_BUCKET),
-    'export': _s3.get_bucket(settings.AWS_S3_EXPORT_BUCKET),
-    'builds': _s3.get_bucket(settings.AWS_S3_BUILDS_BUCKET),
-}
+    _buckets = {
+        'source': _s3.get_bucket(settings.AWS_S3_SOURCE_BUCKET),
+        'export': _s3.get_bucket(settings.AWS_S3_EXPORT_BUCKET),
+        'builds': _s3.get_bucket(settings.AWS_S3_BUILDS_BUCKET),
+    }
+else:
+    _s3 = None
+    _buckets = None
 
 
+def _requires_aws(fn):
+    if settings.AWS_ENABLED:
+        return fn
+    else:
+        def complain(*args, **kwargs):
+            raise Exception("AWS_ENABLED must be True to call %s" % fn.__name__)
+        return complain
+
+
+@_requires_aws
 def read_file(bucket_name, path):
     bucket = _buckets[bucket_name]
     key = bucket.get_key(path)
     return key.get_contents_as_string()
 
 
+@_requires_aws
 def read_file_to_filesystem(bucket_name, path, destination):
     bucket = _buckets[bucket_name]
     key = bucket.get_key(path)
     key.get_contents_to_filename(destination)
 
 
+@_requires_aws
 def save_file(bucket_name, path, value, public=False, content_type='application/octet-stream'):
     bucket = _buckets[bucket_name]
     key = Key(bucket)
@@ -36,6 +52,7 @@ def save_file(bucket_name, path, value, public=False, content_type='application/
     key.set_contents_from_string(value, policy=policy, headers={'Content-Type': content_type})
 
 
+@_requires_aws
 def upload_file(bucket_name, dest_path, src_path, public=False, content_type='application/octet-stream'):
     bucket = _buckets[bucket_name]
     key = Key(bucket)
@@ -48,12 +65,8 @@ def upload_file(bucket_name, dest_path, src_path, public=False, content_type='ap
 
     key.set_contents_from_filename(src_path, policy=policy, headers={'Content-Type': content_type})
 
-# def save_stream(bucket_name, path, stream, file_size):
-#     bucket = _buckets[bucket_name]
-#     key = Key(bucket)
-#     key.key = path
-#     key.set_contents_from_stream(stream, size=file_size)
 
+@_requires_aws
 def get_signed_url(bucket_name, path, headers=None):
     bucket = _buckets[bucket_name]
     key = bucket.get_key(path)
