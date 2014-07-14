ccbd8b366e4ae3a82322395223c80aed0e797971
Katharine Berry <katharine@getpebble.com>
2014-07-14 15:46:27 -0700

    PBL-6490: Sort AppMessage keys by value.

--- ide/views/project.py
+++ ide/views/project.py
@@ -33,7 +33,7 @@ def view_project(request, project_id):
     if project.app_version_label is None:
         project.app_version_label = '1.0'
     send_keen_event('cloudpebble', 'cloudpebble_open_project', request=request, project=project)
-    app_keys = json.loads(project.app_keys).iteritems()
+    app_keys = sorted(json.loads(project.app_keys).iteritems(), key=lambda x: x[1])
     return render(request, 'ide/project.html', {'project': project, 'app_keys': app_keys})
 
 
