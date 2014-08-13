9b20bbd3bdbaf11af50ebeb40edf3f65ac3027df
Katharine Berry <katharine@getpebble.com>
2014-08-12 18:34:04 -0700

    SDK 2.4.1 update (pointless, but we should track it…)

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,7 +43,7 @@ rm arm-cs-tools.tar.bz2
 # Obtain the SDK.
 sudo -u vagrant mkdir sdk2
 pushd sdk2
-    wget --progress=bar:force -O sdk.tar.gz https://s3.amazonaws.com/assets.getpebble.com/sdk2/PebbleSDK-2.4.tar.gz
+    wget --progress=bar:force -O sdk.tar.gz https://s3.amazonaws.com/assets.getpebble.com/sdk2/PebbleSDK-2.4.1.tar.gz
     sudo -u vagrant tar --strip 1 -xzf sdk.tar.gz
     rm sdk.tar.gz
     sudo -u vagrant ln -s ~/arm-cs-tools arm-cs-tools
