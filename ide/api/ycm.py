bb677bb94dd6eb9a384401c878c12690c46d2e0e
Katharine Berry <katharine@getpebble.com>
2014-10-10 16:31:27 -0700

    Hack to restore autocomplete.

--- ide/api/ycm.py
+++ ide/api/ycm.py
@@ -39,7 +39,8 @@ def init_autocomplete(request, project_id):
     server = _choose_ycm_server()
     request = {'files': file_contents}
     # Let's go!
-    result = requests.post('%sspinup' % server, json.dumps(request), headers={'Content-Type': 'application/json'})
+    # FIXME: verify=False is wrong.
+    result = requests.post('%sspinup' % server, json.dumps(request), headers={'Content-Type': 'application/json'}, verify=False)
     response = result.json()
     if response['success']:
         return json_response({'uuid': response['uuid'], 'server': server})
