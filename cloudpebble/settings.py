fc017d4bdc050bec6acf8a8dd5d4e0df01474d68
Katharine Berry <katharine@getpebble.com>
2014-10-13 21:51:45 -0700

    Added translation support + front page.

--- cloudpebble/settings.py
+++ cloudpebble/settings.py
@@ -61,7 +61,13 @@ TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
+
+LANGUAGES = (
+    ('en-gb', 'English'),
+)
+
+LOCALE_PATHS = (os.getcwd() + "/locale",)
 
 SITE_ID = 1
 
@@ -130,8 +136,9 @@ if not DEBUG:
 
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
