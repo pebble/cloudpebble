fe7d076a5ebf41752c0ec14333d1777769cb77bd
Katharine Berry <katharine@getpebble.com>
2014-10-15 04:44:04 -0700

    Fix Editor Settings page in English (oops!)

--- ide/models/user.py
+++ ide/models/user.py
@@ -50,7 +50,7 @@ class UserSettings(IdeModel):
     autocomplete = models.IntegerField(choices=AUTOCOMPLETE_CHOICES, verbose_name=_("Autocompletion"), default=AUTOCOMPLETE_ALWAYS)
     keybinds = models.CharField(max_length=20, verbose_name=_("Keybinds"), choices=KEYBIND_CHOICES, default=KEYBIND_STANDARD)
     theme = models.CharField(max_length=50, verbose_name=_("Theme"), choices=THEME_CHOICES, default='cloudpebble')
-    use_spaces = models.BooleanField(default=True, verbose_name=pgettext_lazy("number of spaces", "Indents"), choices=USE_SPACES_CHOICES)
+    use_spaces = models.BooleanField(default=True, verbose_name=pgettext_lazy("number of spaces", u"Indents"), choices=USE_SPACES_CHOICES)
     tab_width = models.PositiveSmallIntegerField(default=2, verbose_name=_("Tab width"))
 
     # Used for the Pebble ownership transition, when it was set to False.
