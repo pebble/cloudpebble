92af860e764c74c6ec4e160967e1936588496a27
Katharine Berry <katharine@getpebble.com>
2015-02-06 13:44:05 -0800

    Yup.

--- ide/static/ide/js/pebble.js
+++ ide/static/ide/js/pebble.js
@@ -24,7 +24,9 @@ var SharedPebble = new (function() {
         gettext("Petting unicorns…"),
         gettext("Firing missiles…"),
         gettext("Never giving you up…"),
-        gettext("Never letting you down…")
+        gettext("Never letting you down…"),
+        // Translators: do whatever you like with this one.
+        gettext("Harmonising Elements…") // yes.
     ];
 
     this.getEmulator = function() {
