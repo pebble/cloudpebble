d9e5f588851fae266308989830996571cb47c966
Caelyn McAulay <caelyn@gmail.com>
2015-04-18 00:14:50 -0400

    Adding simple tests for url_to_repo and a script to run them

--- ide/tests.py
+++ ide/tests.py
@@ -1,16 +1,31 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Tests in this file can be run with run_tests.py
 """
 
 from django.test import TestCase
+import git
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class UrlToReposTest(TestCase):
+    def test_basic_url_to_repo(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that a simple repo url is correctly recognized.
         """
-        self.assertEqual(1 + 1, 2)
+        username, reponame  = git.url_to_repo("https://github.com/pebble/cloudpebble")
+        self.assertEqual("pebble", username)
+        self.assertEqual("cloudpebble", reponame)
+
+    def test_strange_url_to_repo(self):
+        """
+        Tests that a non-standard repo url is correctly recognized.
+        """
+        username, reponame  = git.url_to_repo("git://github.com:foo/bar.git")
+        self.assertEqual("foo", username)
+        self.assertEqual("bar", reponame)
+
+
+    def test_bad_url_to_repo(self):
+        """
+        Tests that a entirely different url returns None.
+        """
+        self.assertEqual(None, git.url_to_repo("http://www.cuteoverload.com"))
