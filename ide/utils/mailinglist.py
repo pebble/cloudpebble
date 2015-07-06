83019305ccfe08272fe528c753401c7905dd1e16
Cherie Williams <cherie@getpebble.com>
2015-07-06 13:34:53 -0500

    Remove exception for env var & fix naming

--- ide/utils/mailinglist.py
+++ ide/utils/mailinglist.py
@@ -10,11 +10,11 @@ mailchimp_default_list_id = settings.MAILCHIMP_LIST_ID
 
 def add_user(user, mailing_list_id=None):
     if mailchimp_api is None:
-        raise KeyError("Missing MAILCHIMP_API_KEY environment variable")
+        logging.error("Missing MAILCHIMP_API_KEY environment variable")
 
     list_id = mailing_list_id or mailchimp_default_list_id
     if list_id is None:
-        raise KeyError("Missing MAILCHIMP_LIST_ID environment variable")
+        logging.error("Missing MAILCHIMP_LIST_ID environment variable")
 
     try:
         response = mailchimp_api.lists.subscribe(list_id,
