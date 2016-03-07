c21ba6bbb6c1ecbadbd215a07a91237e861713b4
Katharine Berry <katharine@getpebble.com>
2016-03-07 13:41:27 -0800

    Resurrect old firmware warning.

--- ide/static/ide/js/compile.js
+++ ide/static/ide/js/compile.js
@@ -1,5 +1,5 @@
 CloudPebble.Compile = (function() {
-    var MINIMUM_INSTALL_VERSION = "v2.7";
+    var MINIMUM_INSTALL_VERSION = "v3.10";
 
     var COMPILE_SUCCESS_STATES = {
         1: {english: gettext("Pending"), cls: "info", label: 'info'},
