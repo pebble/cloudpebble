78bb9ac5262054159cbcc9803277b97d193ea1c6
Joseph Atkins-Turkish <joeat@pebble.com>
2016-03-22 17:25:36 -0700

    Improved Ajax.Wrap with ability to check for a custom 'success' key in JSON result.

--- root/static/common/js/ajax.js
+++ root/static/common/js/ajax.js
@@ -1,28 +1,33 @@
 Ajax = {
-    Wrap: function(deferred) {
-        /** Wrap a jQuery Ajax deferred object in an ES6-compliant promise.
-         *
-         * The promise will fail if the HTTP request fails, _or_ if the request
-         * returns a JSON object containing "success: false".
-         *
-         * The rejection will be an Error instance, and may include properties from the jqXHR 
-         * object if the HTTP request returned an an error code.
-         * Being an Error, it will always have a .message property with some kind of message.
-         *
-         * If the server's JSON object contains a .error property, .message will be set to that.
-         * Otherwise, .message will come from jQuery's "errorThrown" parameter. Either way,
-         * the object will have a toString() function which just returns .messsage
-         */
+    /** Wrap a jQuery Ajax deferred object in an ES6-compliant promise.
+     *
+     * The promise will fail if the HTTP request fails, _or_ if the request
+     * returns a JSON object containing "success: false".
+     *
+     * The rejection will be an Error instance, and may include properties from the jqXHR
+     * object if the HTTP request returned an an error code.
+     * Being an Error, it will always have a .message property with some kind of message.
+     *
+     * If the server's JSON object contains a .error property, .message will be set to that.
+     * Otherwise, .message will come from jQuery's "errorThrown" parameter. Either way,
+     * the object will have a toString() function which just returns .messsage
+     *
+     * @param {jQuery.Deferred} deferred a jQuery Deferred object
+     * @param {string} [success_key=success] the name of the key in the JSON result object
+     *                  which must have a truthy value for the request to be considered successful.
+     * @returns {Promise}
+     */
+    Wrap: function(deferred, success_key) {
+        if (typeof success_key === "undefined") success_key = 'success';
         return new Promise(function (resolve, reject) {
             deferred.then(function (data) {
-                if (_.isObject(data) && !data.success) {
+                if (_.isObject(data) && !!success_key && !data[success_key]) {
                     reject(data.error ? new Error(data.error) : new Error(gettext("Unknown error")));
                 }
                 else {
                     resolve(data);
                 }
             }).fail(function (jqXHR, textStatus, errorThrown) {
-                // TODO: consider subclassing
                 if (textStatus == 'abort') {
                     resolve();
                 }
