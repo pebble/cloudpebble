bbf0e79e6d71802cbe64e368f51514de068cf7f5
Joseph Atkins-Turkish <joeat@pebble.com>
2016-06-01 14:47:04 -0700

    Re-implemented FakeS3.upload_file with tmp dir assertion

--- utils/fakes.py
+++ utils/fakes.py
@@ -1,3 +1,7 @@
+import tempfile
+import os.path
+
+
 class FakeRedis(object):
     """ Essentially just a dictionary accessed via 'get' and 'set' methods """
 
@@ -16,6 +20,7 @@ class FakeRedis(object):
 
 class FakeS3(object):
     """ Essentially just a dictionary where the keys are tuples of (bucket_name, path) """
+
     def __init__(self):
         self.dict = {}
         self.last_key = None
@@ -38,7 +43,10 @@ class FakeS3(object):
         del self.dict[(bucket_name, path)]
 
     def read_file_to_filesystem(self, bucket_name, path, destination):
-        raise NotImplementedError("S3 Filesystem operations are not allowed during tests")
+        raise NotImplementedError("FakeS3 local-filesystem write operations are not allowed.")
 
     def upload_file(self, bucket_name, path, src_path, **kwargs):
-        raise NotImplementedError("S3 Filesystem operations are not allowed during tests")
+        if not os.path.abspath(src_path).startswith(tempfile.gettempdir()):
+            raise ValueError("FakeS3 local-filesystem read operations may only read from temporary directories.")
+        with open(src_path, 'r') as f:
+            self.save_file(bucket_name, path, f.read())
