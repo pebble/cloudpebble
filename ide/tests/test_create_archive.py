1fd05a97b57e7ab4566f2221443eca0d61fbad91
Joseph Atkins-Turkish <joeat@pebble.com>
2016-06-07 11:38:02 -0700

    Fixed all tests.

--- ide/tests/test_create_archive.py
+++ ide/tests/test_create_archive.py
@@ -14,7 +14,7 @@ class ExportTester(CloudpebbleTestCase):
         fake_s3.reset()
         self.login(options)
         self.project = Project.objects.get(pk=self.project_id)
-        SourceFile.objects.create(project=self.project, file_name="main.c", target="app").save_file('file_contents')
+        SourceFile.objects.create(project=self.project, file_name="main.c", target="app").save_text('file_contents')
 
     def run_test(self, expected_filenames, options=None):
         self.createProject(options)
@@ -26,7 +26,7 @@ class ExportTester(CloudpebbleTestCase):
 
 @override_settings(NPM_MANIFEST_SUPPORT='')
 @mock.patch('ide.tasks.archive.s3', fake_s3)
-@mock.patch('ide.models.files.s3', fake_s3)
+@mock.patch('ide.models.s3file.s3', fake_s3)
 class TestExportWithoutNPMSupport(ExportTester):
     def test_export_sdk_3_project(self):
         """ With package.json support off, check that SDK3 projects are exported with appinfo.json files """
@@ -39,7 +39,7 @@ class TestExportWithoutNPMSupport(ExportTester):
 
 @override_settings(NPM_MANIFEST_SUPPORT='yes')
 @mock.patch('ide.tasks.archive.s3', fake_s3)
-@mock.patch('ide.models.files.s3', fake_s3)
+@mock.patch('ide.models.s3file.s3', fake_s3)
 class TestExportWithNPMSupport(ExportTester):
     def test_export_sdk_3_project(self):
         """ With package.json support off, check that SDK3 projects are exported with package.json files """
