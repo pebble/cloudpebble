41fc7bead6374bc0248a34c39b9acee980ab9741
Katharine Berry <katharine@getpebble.com>
2016-08-16 18:50:50 -0700

    what's new?

--- ide/utils/whatsnew.py
+++ ide/utils/whatsnew.py
@@ -44,6 +44,7 @@ NEW_THINGS = [
     ["Pebble.js can now build Chalk apps for Pebble Time Round. You can enable this in the settings of your pebble.js project."],
     ["We now have support for using packages, enabling easier code sharing! Check out <a href='https://developer.pebble.com/blog/2016/06/07/pebble-packages/'>our blog post</a>."],
     ["You can now develop your own packages in CloudPebble! Select the Pebble Package project type, and then test your package by including it from your other projects in the Dependency pane."],
+    ["Write watchfaces with <a href='https://developer.pebble.com/blog/2016/08/15/introducing-rockyjs-watchfaces/'>JavaScript on the Freakin' Watch</a>!"],
 ]
 
 
