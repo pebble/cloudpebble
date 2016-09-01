2af6939ee12314945768f94e6e1b554c8e1efbe8
Joseph Atkins-Turkish <joeat@pebble.com>
2016-09-01 13:58:27 -0700

    Remove the duplicated 'Compilation' shortcut.

--- ide/static/ide/js/compile.js
+++ ide/static/ide/js/compile.js
@@ -179,7 +179,6 @@ CloudPebble.Compile = (function() {
         commands[gettext("Show Phone Logs")] = function() { show_app_logs(ConnectionType.Phone); };
         commands[gettext("Show Emulator Logs")] = function() { show_app_logs(ConnectionType.Qemu); };
         commands[gettext("Show Last Build Log")] = function() {show_build_log(mLastBuild.id)};
-        commands[gettext("Compilation")] = function() { show_compile_pane();};
         commands[gettext("Clear App Logs")] = function() { show_clear_logs_prompt(); };
         commands[gettext("Take Screenshot")] = function() { take_screenshot(); };
         CloudPebble.FuzzyPrompt.AddCommands(commands);
