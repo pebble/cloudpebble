616a87402f4075e881ae9b5a9ddd17be1ad7031b
Katharine Berry <katharine@getpebble.com>
2016-09-29 14:17:32 -0700

    SDK 4.1.1

--- fabfile.py
+++ fabfile.py
@@ -138,7 +138,7 @@ def update_qemu_images(sdk_version):
     with lcd(get_project_path("tintin")):
         local("git checkout v%s" % sdk_version)
 
-    build_qemu_image("bb2", "aplite")
+    # build_qemu_image("bb2", "aplite")
     build_qemu_image("snowy_bb2", "basalt")
     build_qemu_image("spalding_bb2", "chalk")
     build_qemu_image("silk_bb", "diorite")
